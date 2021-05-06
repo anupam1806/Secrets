a12ee17cd32676f3e027e4412f8a163830973842
Anupam <anupam.saini18@gmail.com>
2021-05-06 14:48:19 +0530

    Third commit

--- app.js
+++ app.js
@@ -210,14 +210,6 @@ app.post("/register",function(req,res){
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
-
-
-app.listen(port,function(){
-    console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
